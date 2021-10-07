4d63b1cf2cd78e0c5e0ead898889805bfb49e15c
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2021-10-07 14:41:54 -0300

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,83 @@
+#  ISSUE ONE 
+
+class Restaurant(): 
+
+  """" 
+  1.Initialize the class with the attributes
+  name, type_restaurant, address. This info comes through the paramerters.
+  2. add and atribute called number_people_served and 
+  initialize it with zero.
+  """
+
+  def __init__(self,            ):
+     # write your code here  
+    
+
+
+
+
+
+  def print_restaurant_info(self): 
+    """ This method prints the restaurant 
+    info.
+    Include the atrributes and add
+    any other info you like  
+    make sure proper capitalization is used 
+    """
+    # write your code here 
+
+
+
+    
+  def set_number_people_served(self,     ):
+    """ this methods recieves the 
+    number of people served 
+    and adds it to the attribute number_people_served """
+  
+  #write your code here
+     
+
+  def print_number_people_served(self,     ):
+  # write your code here 
+
+
+
+
+  # ISSUE TWO    
+  
+class CoffeeStore(      ):
+    """  Make sure this class inherits Restaurant
+     """
+
+  def __init__(self,     ): 
+    """initialize it  with an attribute called name and using a variable called
+    restaurant_type with the default value 'coffe_store'. These are received through parameters. Make sure to call the super() method and pass the right parameters"""
+
+    """
+    add an attribute called coffe_types. This will store a list with at least 3 types of coffe of your choice"""
+     
+     # write your code here 
+
+
+
+
+  def print_coffe_store_info(self):
+    """
+    This methods prints the coffe store info 
+    use a for loop to print the coffy types in a 
+    bullet list format
+    Add any other info you like 
+    """
+
+
+
+
+# ISSUE 3     
+""" Write the main part of the program
+make an instace of the class Restaurante
+and call the methods
+Make an instance of the class CoffeeStore
+call 
+"""
+
+# write your code here  
