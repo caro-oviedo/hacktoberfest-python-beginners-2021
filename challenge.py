23dce6f188850b0dc54c4f25274aad4b8c4bf733
NarayanAdithya <narayanadithya1234@gmail.com>
2021-10-09 01:07:13 +0530

    Added Gitignore and Pytest

--- challenge.py
+++ challenge.py
@@ -38,41 +38,41 @@ class Restaurant():
 
 
 
-# ISSUE TWO    
+# # ISSUE TWO    
   
-class CoffeeStore(      ):
-    """  Make sure this class inherits Restaurant
-     """
-
-  def __init__(self,     ): 
-    """initialize it  with an attribute called name and using a variable called
-    restaurant_type with the default value 'coffe_store'. These are received through parameters. Make sure to call the super() method and pass the right parameters"""
-
-    """
-    add an attribute called coffe_types. This will store a list with at least 3 types of coffe of your choice"""
-     
-     # write your code here 
-
-
-
-
-  def print_coffe_store_info(self):
-    """
-    This methods prints the coffe store info 
-    use a for loop to print the coffy types in a 
-    bullet list format
-    Add any other info you like 
-    """
+# class CoffeeStore(      ):
+#   """  
+#   Make sure this class inherits Restaurant
+#   """
+#   def __init__(self,    ): 
+#   """
+#   initialize it  with an attribute called name and using a variable called
+#   restaurant_type with the default value 'coffe_store'. These are received through parameters. Make sure to call the super() method and pass the right parameters"""
+#   """
+#   add an attribute called coffe_types. This will store a list with at least 3 types of coffe of your choice"""
+    
+#     # write your code here 
 
 
 
 
-# ISSUE 3     
-""" Write the main part of the program
-make an instace of the class Restaurante
-and call the methods
-Make an instance of the class CoffeeStore
-call 
-"""
+#   def print_coffe_store_info(self):
+#     """
+#     This methods prints the coffe store info 
+#     use a for loop to print the coffy types in a 
+#     bullet list format
+#     Add any other info you like 
+#     """
 
-# write your code here  
+
+
+
+# # ISSUE 3     
+# """ Write the main part of the program
+# make an instace of the class Restaurante
+# and call the methods
+# Make an instance of the class CoffeeStore
+# call 
+# """
+
+# # write your code here  
